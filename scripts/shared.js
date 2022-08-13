c99bbc027b9a020baad09846191d80de06406106
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2022-08-13 10:51:46 -0400

    DfS

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,28 +1,78 @@
+// const hamburger = document.getElementById('homepage__nav-links__hamburger');
+// const homepage = document.getElementById('homepage');
+// const navLinksWrapper = document.getElementById('homepage__nav-links');
+// const html = document.querySelector('html');
+
+// document
+//   .getElementById('homepage__nav-links__secret-hamburger-wrapper')
+//   .addEventListener('click', () => {
+//     navLinksWrapper.style.display = 'none';
+//   });
+
+// hamburger.addEventListener('click', () => {
+//   if (html.clientWidth > 900) {
+//     if (hamburger.src.includes('open')) {
+//       hamburger.src.includes('white')
+//         ? (hamburger.src = '../icons/hamburger-close-white.png')
+//         : (hamburger.src = '../icons/hamburger-close-black.png');
+//     } else {
+//       hamburger.src.includes('black')
+//         ? (hamburger.src = '../icons/hamburger-open-black.png')
+//         : (hamburger.src = '../icons/hamburger-open-white.png');
+//     }
+//   }
+
+//   navLinksWrapper.style.display = 'block';
+// });
+
+// window.onscroll = () => {
+//   const nav = document.getElementById('homepage__nav');
+//   const name = document.getElementById('homepage__nav__name');
+//   const logo = document.getElementById('logo');
+//   const navLinks = document.querySelectorAll('.homepage__nav-links__link');
+
+//   if (homepage.getBoundingClientRect().top < 0) {
+//     navLinks.forEach(navLink => (navLink.style.color = 'black'));
+//     hamburger.src.includes('open')
+//       ? (hamburger.src = '../icons/hamburger-open-black.png')
+//       : (hamburger.src = '../icons/hamburger-close-black.png');
+//     nav.style.backgroundColor = '#fff';
+//     name.style.display = 'none';
+//     logo.style.display = 'block';
+//     nav.style.padding = '20px';
+//   } else {
+//     navLinks.forEach(navLink => (navLink.style.color = '#fff'));
+//     hamburger.src.includes('open')
+//       ? (hamburger.src = '../icons/hamburger-open-white.png')
+//       : (hamburger.src = '../icons/hamburger-close-white.png');
+//     nav.style.backgroundColor = 'transparent';
+//     logo.style.display = 'none';
+//     nav.style.padding = '40px';
+//     name.style.display = 'block';
+//   }
+// };
+
 const homepage = document.getElementById('homepage');
 const navLinksWrapper = document.getElementById('homepage__nav-links');
 const html = document.querySelector('html');
 const hamburger = document.getElementById('homepage__nav-links__hamburger');
 const overlay = document.getElementById('overlay');
 
+const removeSideNav = () => {
+  navLinksWrapper.style.display = 'none';
+  overlay.style.display = 'none';
+};
+
 document
   .getElementById('homepage__nav-links__secret-hamburger-wrapper')
-  .addEventListener('click', () => {
-    navLinksWrapper.style.animation = 'slideRight 1s';
-    navLinksWrapper.style.display = 'none';
-  });
+  .addEventListener('click', removeSideNav);
 
 const setHamburger = fileLevel => {
   hamburger.addEventListener('click', () => {
     navLinksWrapper.style.animation = 'slideLeft 0.5s';
     navLinksWrapper.style.display = 'block';
     overlay.style.display = 'block';
-
-    window.addEventListener('click', e => {
-      if (e.target.id !== navLinksWrapper.id && e.target.id !== hamburger.id) {
-        navLinksWrapper.style.display = 'none';
-        overlay.style.display = 'none';
-      }
-    });
+    overlay.addEventListener('click', removeSideNav);
   });
 };
 
